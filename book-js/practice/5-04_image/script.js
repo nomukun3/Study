1fda3152fa30230dd2140b5ca54f8b6556f31461
Yokota Seiki <hw17a207@gmail.com>
2020-03-16 11:06:33 +0900

    こここ

--- book-js/practice/5-04_image/script.js
+++ book-js/practice/5-04_image/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const thumbs = document.querySelectorAll('.thumb');
-thumbs.forEach(function (item, index) {
-  item.onclick = function () {
+thumbs.forEach(function(item, index) {
+  item.onclick = function() {
     console.log(this.dataset.image);
   };
 });
+
+
