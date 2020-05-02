ee7be61b3916fab83341adcebf9b7af1854b5f3c
Yokota Seiki <hw17a207@gmail.com>
2020-05-03 08:53:57 +0900

    2020_05_03

--- htmlgame/007-javascript/css-js-timer/script.js
+++ htmlgame/007-javascript/css-js-timer/script.js
@@ -1,3 +1,23 @@
 'use strict';
 
+var timerId;
 
+function timeoutStart() {
+  timerId = setTimeout(whatime, 3000);
+}
+
+function timeoutStop() {
+  clearTimeout(timerId);
+}
+
+function intervalStart() {
+  timerId = setInterval(whatime, 0);
+}
+
+function intervalStop() {
+  clearInterval(timerId);
+}
+
+function whatime() {
+  document.getElementById("info").textContent = new Date();
+}
