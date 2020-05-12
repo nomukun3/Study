50fca2d60fbb69f7dd5ab35ec2e8ff1a43a66edd
Yokota Seiki <hw17a207@gmail.com>
2020-05-12 11:38:19 +0900

    2020_05_12

--- htmlgame/100-game/script.js
+++ htmlgame/100-game/script.js
@@ -1,9 +1,61 @@
 'use strict';
 
-var ctx;
+let ctx, x = 1100, y = 150 + 50;
+
+
+let canvas = document.getElementById("main");
+ctx = canvas.getContext("2d");
+
 
 function init() {
-  var canvas = document.getElementById("main");
-
-  ctx = canvas.getContext("2d");
+  setInterval(main, 10);
 }
+
+
+
+
+
+
+
+function main() {
+  let onpu = document.getElementById("onpu");
+  
+  if (x >= 100) {
+    ctx.clearRect(x, y, onpu.width, onpu.height);
+    x=x-10;
+    verticalline();
+    sedeline();
+    ctx.drawImage(onpu, x, y);
+  }else{
+    ctx.clearRect(x, y, onpu.width, onpu.height);
+    verticalline();
+    sedeline();
+  }
+}
+
+
+// 背景の線
+function verticalline() {
+  ctx.strokeStyle = "#696969";
+  ctx.lineWidth = 4;
+  ctx.beginPath();
+  ctx.moveTo(150, 100);
+  ctx.lineTo(150, 600);
+  ctx.stroke();
+}
+
+
+function sedeline() {
+  for (let i = 0; i < 5; ++i) {
+    ctx.strokeStyle = "#696969";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(50 , 150 + (30 * i) );
+    ctx.lineTo(1200, 150 + (30 * i) );
+    ctx.stroke();
+  }
+}
+
+
+
+
