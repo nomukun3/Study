3aa961fd06d0642ea8b40291f38dda8aaf004a11
hw17a207yokota <53849733+hw17a207yokota@users.noreply.github.com>
2020-06-21 04:16:46 +0900

    Delete updatenomu.zsh

--- web/updatenomu.zsh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-find ~/Desktop/ramen/web/www/ -name ".DS_Store" -print -exec rm "{}" \;
-rsync -avz --delete -e "ssh -i ~/.ssh/sakurapass" ~/Desktop/ramen/web/www/* nomutan5@nomutan5.sakura.ne.jp:~/www/
